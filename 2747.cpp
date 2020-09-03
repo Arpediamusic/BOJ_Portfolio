a2cc10290778982a1b33a2ebdc3fdb9e25c53e91
Arpedia <63240619+Arpediamusic@users.noreply.github.com>
2020-09-03 16:10:00 +0900

    Create 2747.cpp
    BOJ 2727 피보나치 수 C++ 코드입니다.

---
+++ 2747.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <stack>
+using namespace std;
+#define ll long long int
+
+ll memo[50];
+
+ll fibo(int q) {
+    if(!memo[q]) {
+        if(q<=2) {
+            memo[q]=1;
+        } else {
+            memo[q]=(fibo(q-1)+fibo(q-2));
+        }
+    }
+    return memo[q];
+}
+
+int main() {
+    int N;
+    cin >> N;
+    cout << fibo(N);
+}
